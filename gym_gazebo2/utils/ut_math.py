5110e8d53e596996acc75f6298f68525d84c8f33
AngryCodes0404 <angrybird.codes@gmail.com>
2025-11-26 12:11:41 -0500

    fix type error

--- gym_gazebo2/utils/ut_math.py
+++ gym_gazebo2/utils/ut_math.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 
+
 def rmseFunc(eePoints):
     """
     Computes the Residual Mean Square Error of the difference between current and desired
@@ -8,6 +9,7 @@ def rmseFunc(eePoints):
     """
     rmse = np.sqrt(np.mean(np.square(eePoints), dtype=np.float32))
     return rmse
+
 
 def computeReward(rewardDist, rewardOrientation=0, collision=False):
     alpha = 5
@@ -17,14 +19,18 @@ def computeReward(rewardDist, rewardOrientation=0, collision=False):
     eta = 0.03
     done = 0.02
 
-    distanceReward = (math.exp(-alpha * rewardDist) - math.exp(-alpha)) \
-     / (1 - math.exp(-alpha)) + 10 * (math.exp(-alpha/done * rewardDist) - math.exp(-alpha/done)) \
-     / (1 - math.exp(-alpha/done))
-    orientationReward = (1 - (rewardOrientation / math.pi)**beta + gamma) / (1 + gamma)
+    distanceReward = (math.exp(-alpha * rewardDist) - math.exp(-alpha)) / (
+        1 - math.exp(-alpha)
+    ) + 10 * (math.exp(-alpha / done * rewardDist) - math.exp(-alpha / done)) / (
+        1 - math.exp(-alpha / done)
+    )
+    orientationReward = (1 - (rewardOrientation / math.pi) ** beta + gamma) / (
+        1 + gamma
+    )
 
     if collision:
         rewardDist = min(rewardDist, 0.5)
-        collisionReward = delta * (2 * rewardDist)**eta
+        collisionReward = delta * (2 * rewardDist) ** eta
     else:
         collisionReward = 0
 
